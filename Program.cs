2671be82eba9d40a2dff7dc1b4e2b3d57a82a045
bhmaharipriya <bhumaharipriya25@gmail.com>
2021-11-11 17:05:40 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Demo10
+{
+    class Customer
+{
+    string _firstname;
+    string _lastname;
+
+    public Customer(string firstname, string lastname)
+    {
+        _firstname = firstname;
+        _lastname = lastname;
+    }
+
+    public string PrintFullName()
+    {
+        return _firstname + " " + _lastname;
+    }
+
+    ~Customer()
+    {
+        //clean-up code
+    }
+}
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Customer c1 = new Customer("Nazeeruddin", "Shaik");
+        Console.WriteLine(c1.PrintFullName());
+        }
+    }
+}
